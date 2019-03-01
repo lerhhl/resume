10ecac0edb1985b259db7b4842f568d2a732db5d
Hwee Lern <hweelern@sgag.sg>
2019-03-01 09:52:00 +0800

    Change navbar to side floating navbar

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
+    <div id="side-nav">
+      <router-link to="/">Home</router-link>
       <router-link to="/about">About</router-link>
     </div>
     <router-view/>
@@ -16,15 +16,34 @@
   text-align: center;
   color: #2c3e50;
 }
-#nav {
+#side-nav {
+  position: fixed;
+  top: 20vh;
+  left: 0px;
+  display: flex;
+  flex-direction: column;
   padding: 30px;
 }
-
-#nav a {
+#side-nav a {
   font-weight: bold;
   color: #2c3e50;
+  text-decoration: none;
+  border: 1px solid black;
+  width: 80px;
+  background-color: lightblue;
+  box-shadow: 2px 2px #888888;
 }
-
+#side-nav a:nth-child(even) {
+  border-top: none;
+}
+#side-nav a:first-child {
+  border-top-left-radius: 5px;
+  border-top-right-radius: 5px;
+}
+#side-nav a:last-child {
+  border-bottom-left-radius: 5px;
+  border-bottom-right-radius: 5px;
+}
 #nav a.router-link-exact-active {
   color: #42b983;
 }
